55a48d7fa4836f265f708c71fe0f06fd115842b0
Gaping Joshua <k.j10mars2000@gmail.com>
2023-11-27 17:30:11 +0100

    created a middleware and a function for errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs'
 import User from '../models/User.model.js'
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const {username, email, password} = req.body
     const hashedPassword = bcryptjs.hashSync(password, 10) // 10 is salt number;salt is just what is going to combine with our password and make it encrypted
     const newUser = new User({username, email, password: hashedPassword})
@@ -9,6 +9,6 @@ export const signup = async (req, res) => {
         await newUser.save() 
         res.status(201).json('user created succesfully')
       } catch(error) {
-        res.status(500).json(error.message);
+        next(error);
       }
 }
