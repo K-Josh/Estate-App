0f5ac5ecb687a93a4970ac995b07fb05562bfb61
Gaping Joshua <k.j10mars2000@gmail.com>
2024-01-03 16:04:54 +0100

    signup page functionality complete

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,7 @@
 # in the backend, from the auth.controller we obtained the username,email,password in the req.body.
-Then it will be saved in a function called newUser from user.model. Then newUser was saved using newUser.save().
+=> Then it will be saved in a function called newUser from user.model. Then newUser was saved using newUser.save().
 using the .save() will be slow on saving the user's details so we had to put an await in the code line await newUser.save(){so the code will stay in this line until it finishes before going to the next line} and the whole function had to be changed from a synchronous function to an unsynchronous function using the (async).
 Then a res.status().json was sent to the browser to indicate if the user was created successfully.
+=> We then created a middleware solely for handling errors this method prevents to many api routes in our code. so creating the middleware in the server takes 4 parameters (err, req, res, next). we create a statusCode
 #  we add a proxy in the vite config so that when we fetch the data from the user input it goes directly to the link specified
 <!-- we changed the formData to a string -->
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,39 +1,54 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 
 const SignUp = () => {
   const [formData, setFormData] = useState({})
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
   const handleChange = (e)=>{
     setFormData({
       ...formData, 
       [e.target.id]: e.target.value
-    })
+    }) 
   }
   const handleSubmit = async (e)=>{
     e.preventDefault()
-    setLoading(true)
-    const res = await fetch('/api/auth/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data); 
+    try {
+      setLoading(true)
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data); 
+      if(data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+    } catch (error) {
+      setLoading(false)
+      setError(error.message)
+    }
   }
-
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='font-semibold text-center my-7 justify-center text-3xl '>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type='text' placeholder='Enter username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        <input type='email' placeholder='Enter email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type='password' placeholder='Enter password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        <button className='text-white p-2 bg-green-600 rounded-lg uppercase hover:opacity-75 disabled:opacity-80'>Sign Up</button>
+        <input type='text' placeholder='Enter username' className='border p-3 rounded-lg focus:outline-green-600' id='username' onChange={handleChange}/>
+        <input type='email' placeholder='Enter email' className='border p-3 rounded-lg focus:outline-red-600' id='email' onChange={handleChange}/>
+        <input type='password' placeholder='Enter password' className='border p-3 rounded-lg focus:outline-yellow-500' id='password' onChange={handleChange}/>
+        <button disabled={loading} className='text-white p-2 bg-green-600 rounded-lg uppercase hover:opacity-75 disabled:opacity-80'>
+        {loading ? 'Loading...' : 'Sign up'}
+        </button>
       </form>
       <div className='flex gap-2 mt-4'>
         <p className='text-[16px]'>Have an account?</p>
@@ -41,8 +56,8 @@ const SignUp = () => {
           <span className='text-green-500'>Sign in</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
-}
-
+ }
 export default SignUp
